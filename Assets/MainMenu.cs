972d1941352e339a11f00733298028538004035a
Göksun Gürel <goksungurell@gmail.com>
2025-12-30 21:30:45 +0300

    Update MainMenu.cs

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -6,7 +6,7 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadSceneAsync(3);
+        SceneManager.LoadSceneAsync(7);
     }
 
     public void QuitGame()
